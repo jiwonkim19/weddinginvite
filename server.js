edcdce93293a8a975d09652facf50b26f7083fe7
Ji-Won Kim <jiwonkim89@gmail.com>
2022-01-15 13:23:40 -0800

    get and post req

--- server.js
+++ server.js
@@ -9,3 +9,18 @@ app.listen(port, () => {
   console.log('Server is running at http://localhost:' + port);
 });
 
+let serverObj = []
+
+app.get('/bar', (req, res) => {
+  res.send(serverObj)
+})
+
+app.post('/bar', (req, res) => {
+  const example1 = req.body
+  serverObj.push(example1)
+})
+
+app.get('/foo', (req, res) => {
+  const test = {foo : 'bar'}
+  res.send(test)
+})
