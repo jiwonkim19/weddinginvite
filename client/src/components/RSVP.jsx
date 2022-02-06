314b993079cea905f28f8eb5e3e2d092d31b6911
Ji-Won Kim <jiwonkim89@gmail.com>
2022-02-05 23:52:16 -0800

    add basic checkbox

--- client/src/components/RSVP.jsx
+++ client/src/components/RSVP.jsx
@@ -3,7 +3,58 @@ import { useParams, useNavigate } from "react-router-dom";
 import {
   Form,
   Button,
+  Dropdown,
+  ToggleButton,
+  ButtonGroup
 } from 'react-bootstrap';
+
+const RsvpCheck = () => {
+  return (
+    <Form>
+    <Form.Group>
+    <Form.Label>RSVP</Form.Label>
+    <Form.Check
+    inline
+    type="checkbox"
+    label="Accept"/>
+    <Form.Check
+    inline
+     type="checkbox"
+    label="Decline"/>
+  </Form.Group>
+  </Form>
+  )
+}
+
+const FoodCheck = () => {
+  return (
+    <Form>
+    <Form.Label>Lunch Selection</Form.Label>
+    <Form.Group>
+    <Form.Check
+    inline
+     type="checkbox"
+    label="Beef"/>
+    <Form.Check
+    inline
+     type="checkbox"
+    label="Chicken"/>
+     <Form.Check
+     disabled={false}
+     inline
+     type="checkbox"
+    label="Fish"/>
+     <Form.Check
+     disabled={true}
+     inline
+     type="checkbox"
+    label="Vegetarian"/>
+  </Form.Group>
+  </Form>
+ 
+
+  )
+}
 
 const RSVP = () => {
   const { name } = useParams();
@@ -15,68 +66,10 @@ const RSVP = () => {
   const navigate = useNavigate();
 
   return (
-
-    <Form  style={{
-      textAlign: "center",
-      maxWidth: "950px",
-      margin: "0 auto",
-      border: "1px solid #e6e6e6",
-      padding: "40px 25px",
-      marginTop: "50px"
-  }}
->
-      <Form.Group className="mb-3">
-        <Form.Label>Hi {name}! We would love to see you!</Form.Label>
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>Attendance</Form.Label>
-        <Form.Select
-          onChange={e => {
-            setRsvp({
-              ...rsvp,
-              attendance: e.target.value
-            })
-          }}
-        >
-          <option>Pick one...</option>
-          <option value="accept">Accept</option>
-          <option value="decline">Decline</option>
-        </Form.Select>
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>Lunch Preference</Form.Label>
-        <Form.Select
-          onChange={e => {
-            setRsvp({
-              ...rsvp,
-              food: e.target.value
-            })
-          }}
-        >
-          <option>Pick one...</option>
-          <option value="beef">Beef</option>
-          <option value="chicken">Chicken</option>
-          <option value="fish">Fish</option>
-          <option value="vegetarian">Vegetarian</option>
-        </Form.Select>
-      </Form.Group>
-      <Button
-        onClick={(e)=> {
-          e.preventDefault();
-
-          // TODO: handle failed submission
-          fetch(`/rsvp/${name}`, {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ rsvp })
-          });
-
-          navigate('/confirmed');
-        }}
-      >
-        Submit
-      </Button>
-    </Form>
+    <div>
+<RsvpCheck />
+<FoodCheck />
+    </div>
   );
 };
 
