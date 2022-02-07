c25769d1b379e44cb036b3568497b134ddad7be4
Ji-Won Kim <jiwonkim89@gmail.com>
2022-02-06 17:17:59 -0800

    prefilled radio buttons

--- client/src/components/RSVP.jsx
+++ client/src/components/RSVP.jsx
@@ -8,69 +8,107 @@ import {
   ButtonGroup
 } from 'react-bootstrap';
 
-const RsvpCheck = () => {
+
+const RsvpCheck = ({ attendance,setAttendance }) => {
   return (
     <Form>
-    <Form.Group>
-    <Form.Label>RSVP</Form.Label>
-    <Form.Check
-    inline
-    type="checkbox"
-    label="Accept"/>
-    <Form.Check
-    inline
-     type="checkbox"
-    label="Decline"/>
-  </Form.Group>
-  </Form>
+      <Form.Group>
+        <Form.Label>RSVP</Form.Label>
+        <Form.Check
+          inline
+          type="radio"
+          label="Accept"
+          name='group1'
+          checked={attendance === 'accept'}
+          onClick={() => {
+            setAttendance('accept')
+          }}
+        />
+        <Form.Check
+          inline
+          type="radio"
+          label="Decline"
+          name='group1'
+          checked={attendance === 'decline'}
+          onClick={() => {
+            setAttendance('decline')
+          }}
+        />
+      </Form.Group>
+    </Form>
   )
 }
 
-const FoodCheck = () => {
+const FoodCheck = ({food,setFood}) => {
   return (
     <Form>
-    <Form.Label>Lunch Selection</Form.Label>
-    <Form.Group>
-    <Form.Check
-    inline
-     type="checkbox"
-    label="Beef"/>
-    <Form.Check
-    inline
-     type="checkbox"
-    label="Chicken"/>
-     <Form.Check
-     disabled={false}
-     inline
-     type="checkbox"
-    label="Fish"/>
-     <Form.Check
-     disabled={true}
-     inline
-     type="checkbox"
-    label="Vegetarian"/>
-  </Form.Group>
-  </Form>
- 
+      <Form.Label>Lunch Selection</Form.Label>
+      <Form.Group>
+        <Form.Check
+          inline
+          type="radio"
+          label="Beef"
+          name='group2'
+          checked={food === 'beef'}
+          onClick={() => {
+             setFood('beef')
+           }}/>
+        <Form.Check
+          inline
+          type="radio"
+          label="Chicken"
+          name='group2'
+          checked={food === 'chicken'}
+          onClick={() => {
+            setFood('chicken')
+          }}
+           />
+        <Form.Check
+          inline
+          type="radio"
+          label="Fish"
+          name='group2'
+          checked={food === 'fish'}
+          onClick={() => {
+            setFood('fish')
+          }}
+           />
+        <Form.Check
+          inline
+          type="radio"
+          label="Vegetarian"
+          name='group1'
+          checked={food === 'vegetarian'}
+          onClick={() => {
+            setFood('vegetarian')
+          }}
+           />
+      </Form.Group>
+    </Form>
+
 
   )
 }
 
 const RSVP = () => {
   const { name } = useParams();
-  const [rsvp, setRsvp] = useState({
-    attendance: '',
-    food: '',
-  });
+  // const [rsvp, setRsvp] = useState({
+  //   attendance: attendance,
+  //   food: food,
+  // });
 
   const navigate = useNavigate();
+  const [food,setFood] = useState('beef')
+  const [attendance,setAttendance] = useState('accept')
 
   return (
     <div>
-<RsvpCheck />
-<FoodCheck />
+      <RsvpCheck attendance={attendance} setAttendance={setAttendance} />
+      <FoodCheck food={food} setFood={setFood} />
     </div>
   );
 };
 
 export default RSVP;
+
+
